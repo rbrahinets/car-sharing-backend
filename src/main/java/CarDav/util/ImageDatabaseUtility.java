3d5ecdb7709a27ece8c9763325eb539b942db5f8
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-18 14:47:04 +0300

    Add utility for work with image in database

---
+++ src/main/java/CarDav/util/ImageDatabaseUtility.java
@@ -0,0 +1,22 @@
+package CarDav.util;
+
+import CarDav.db.DatabaseTemplate;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+public class ImageDatabaseUtility {
+    public void setImagesToDatabase(List<String> images) throws Exception {
+//        CarRepository productRepository = new ProductRepository(DatabaseTemplate.getJdbcTemplate());
+//        for (int i = 0; i < images.size(); i++) {
+//            Optional<Product> product = productRepository.findById(i + 1);
+//            if (product.isPresent()) {
+//                if (product.get().getImage() == null) {
+//                    byte[] byteImage = ImageUtility.imageToBytes(new File(images.get(i)));
+//                    productRepository.saveImageForProduct(byteImage, i + 1);
+//                }
+//            }
+//        }
+    }
+}
