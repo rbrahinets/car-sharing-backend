46dc4cc05f9581d89b7c0bb993a5a4da9f9df6af
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-18 14:50:53 +0300

    Add repository for user

---
+++ src/main/java/CarDav/repositories/UserRepository.java
@@ -0,0 +1,42 @@
+package CarDav.repositories;
+
+import CarDav.DAO.UserMapper;
+import CarDav.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class UserRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+    public List<User> findAll() {
+        return jdbcTemplate.query(
+                "SELECT * FROM `user`",
+                new UserMapper()
+        );
+    }
+
+    public Optional<User> findById(Long id) {
+        return Optional.of(new User());
+    }
+
+    public User save(User user) {
+        return user;
+    }
+
+    public User update(Long id, User user) {
+        return user;
+    }
+
+    public void deleteById(Long id) {
+
+    }
+}
