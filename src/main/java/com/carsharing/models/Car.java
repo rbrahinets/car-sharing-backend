35c9efe035e2c1c5aefe50322737b368a5ed475d
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-26 16:49:27 +0300

    Update model for car

--- src/main/java/com/carsharing/models/Car.java
+++ src/main/java/com/carsharing/models/Car.java
@@ -1,143 +1,114 @@
 package com.carsharing.models;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
-import java.util.Date;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Car {
-    private int id_car;
-
-    private int id_admin;
-
-    @NotEmpty(message = "Brand shouldn\'d be empty")
+    private int id;
     private String brand;
-
-    @NotEmpty(message = "Model shouldn\'d be empty")
     private String model;
-
-   // @Min(value = 1970, message = "Issue date must be greater then 1970")
-    private Date issue_date;
-
-    @Min(value = 0, message = "Price must be greater then 0")
+    private int year;
     private double price;
-
-    private int id_category;
-
- //   @Pattern(regexp = "^[A-Z]{2} \\d{4} [A-Z]{2}$", message = "Incorrect plate format")
+    private int idCategory;
     private String plate;
-
-    private boolean aveliable;
-
-    private boolean damage;
-
-    private String URLImage;
+    private boolean available;
+    private byte[] image;
 
     public Car() {
     }
 
-    public Car(int id_car, int id_admin, String brand,
-               String model, Date issue_date, double price,
-               int id_category, String plate, boolean aveliable,
-               boolean damage, String URLImage) {
-        this.id_car = id_car;
-        this.id_admin = id_admin;
+    public Car(
+        int id,
+        String brand,
+        String model,
+        int year,
+        double price,
+        int idCategory,
+        String plate,
+        boolean available,
+        byte[] image
+    ) {
+        this.id = id;
         this.brand = brand;
         this.model = model;
-        this.issue_date = issue_date;
+        this.year = year;
         this.price = price;
-        this.id_category = id_category;
+        this.idCategory = idCategory;
         this.plate = plate;
-        this.aveliable = aveliable;
-        this.damage = damage;
-        this.URLImage = URLImage;
+        this.available = available;
+        this.image = image;
     }
 
-    public int getId_car() {
-        return id_car;
-    }
-
-    public void setId_car(int id_car) {
-        this.id_car = id_car;
-    }
-
-    public int getId_admin() {
-        return id_admin;
-    }
-
-    public void setId_admin(int id_admin) {
-        this.id_admin = id_admin;
+    public int getId() {
+        return id;
     }
 
     public String getBrand() {
         return brand;
     }
 
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
     public String getModel() {
         return model;
     }
 
-    public void setModel(String model) {
-        this.model = model;
-    }
-
-    public Date getIssue_date() {
-        return issue_date;
-    }
-
-    public void setIssue_date(Date issue_date) {
-        this.issue_date = issue_date;
+    public int getYear() {
+        return year;
     }
 
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getId_category() {
-        return id_category;
-    }
-
-    public void setId_category(int id_category) {
-        this.id_category = id_category;
+    public int getIdCategory() {
+        return idCategory;
     }
 
     public String getPlate() {
         return plate;
     }
 
-    public void setPlate(String plate) {
-        this.plate = plate;
+    public boolean isAvailable() {
+        return available;
     }
 
-    public boolean isAveliable() {
-        return aveliable;
+    public byte[] getImage() {
+        return image;
     }
 
-    public void setAveliable(boolean aveliable) {
-        this.aveliable = aveliable;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return id == car.id
+            && year == car.year
+            && Double.compare(car.price, price) == 0
+            && idCategory == car.idCategory
+            && available == car.available
+            && Objects.equals(brand, car.brand)
+            && Objects.equals(model, car.model)
+            && Objects.equals(plate, car.plate)
+            && Arrays.equals(image, car.image);
     }
 
-    public boolean isDamage() {
-        return damage;
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, brand, model, year, price, idCategory, plate, available);
+        result = 31 * result + Arrays.hashCode(image);
+        return result;
     }
 
-    public void setDamage(boolean damage) {
-        this.damage = damage;
-    }
-
-    public String getURLImage() {
-        return URLImage;
-    }
-
-    public void setURLImage(String URLImage) {
-        this.URLImage = URLImage;
+    @Override
+    public String toString() {
+        return "Car{" +
+            "id=" + id +
+            ", brand='" + brand + '\'' +
+            ", model='" + model + '\'' +
+            ", year=" + year +
+            ", price=" + price +
+            ", idCategory=" + idCategory +
+            ", plate='" + plate + '\'' +
+            ", available=" + available +
+            '}';
     }
 }
