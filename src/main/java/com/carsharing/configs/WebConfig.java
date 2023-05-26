0b3e5b7d10a71dc36e7d0a57bc4d97a6042041e6
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-26 22:55:42 +0300

    Update configurations for web

--- src/main/java/com/carsharing/configs/WebConfig.java
+++ src/main/java/com/carsharing/configs/WebConfig.java
@@ -2,21 +2,15 @@ package com.carsharing.configs;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/sign-in");
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*")
-            .allowedMethods("*")
+            .allowedOrigins("")
+            .allowedMethods("http://localhost:5173")
             .allowedHeaders("*")
             .allowCredentials(true);
     }
