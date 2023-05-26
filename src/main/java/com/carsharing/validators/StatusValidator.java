2f9a6fc3686e17e20dde86a2665199d4ce3d95de
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-26 19:24:42 +0300

    Add validator for status

---
+++ src/main/java/com/carsharing/validators/StatusValidator.java
@@ -0,0 +1,47 @@
+package com.carsharing.validators;
+
+import com.carsharing.exceptions.NotFoundException;
+import com.carsharing.exceptions.ValidationException;
+import com.carsharing.models.Status;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class StatusValidator {
+    public void validate(Status status, List<Status> statuses) {
+        if (isNameInvalid(status.getName())) {
+            throw new ValidationException("Name is invalid");
+        } else if (isNameAlreadyExist(status.getName(), statuses)) {
+            throw new ValidationException("Name already exist");
+        }
+    }
+
+    public void validate(long id, List<Status> statuses) {
+        List<Long> ids = new ArrayList<>();
+
+        for (Status status : statuses) {
+            ids.add(status.getId());
+        }
+
+        if (!ids.contains(id)) {
+            throw new NotFoundException("Status not found");
+        }
+    }
+
+    private static boolean isNameInvalid(String name) {
+        return name == null
+            || name.isBlank();
+    }
+
+    private boolean isNameAlreadyExist(String name, List<Status> statuses) {
+        List<String> names = new ArrayList<>();
+
+        for (Status status : statuses) {
+            names.add(status.getName());
+        }
+
+        return names.contains(name);
+    }
+}
