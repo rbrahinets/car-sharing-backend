b0449b19fe59ad4e40058444714e98765e8cf45b
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-26 19:24:32 +0300

    Add validator for category

---
+++ src/main/java/com/carsharing/validators/CategoryValidator.java
@@ -0,0 +1,47 @@
+package com.carsharing.validators;
+
+import com.carsharing.exceptions.NotFoundException;
+import com.carsharing.exceptions.ValidationException;
+import com.carsharing.models.Category;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class CategoryValidator {
+    public void validate(Category category, List<Category> categories) {
+        if (isNameInvalid(category.getName())) {
+            throw new ValidationException("Name is invalid");
+        } else if (isNameAlreadyExist(category.getName(), categories)) {
+            throw new ValidationException("Name already exist");
+        }
+    }
+
+    public void validate(long id, List<Category> categories) {
+        List<Long> ids = new ArrayList<>();
+
+        for (Category category : categories) {
+            ids.add(category.getId());
+        }
+
+        if (!ids.contains(id)) {
+            throw new NotFoundException("Category not found");
+        }
+    }
+
+    private static boolean isNameInvalid(String name) {
+        return name == null
+            || name.isBlank();
+    }
+
+    private boolean isNameAlreadyExist(String name, List<Category> categories) {
+        List<String> names = new ArrayList<>();
+
+        for (Category category : categories) {
+            names.add(category.getName());
+        }
+
+        return names.contains(name);
+    }
+}
