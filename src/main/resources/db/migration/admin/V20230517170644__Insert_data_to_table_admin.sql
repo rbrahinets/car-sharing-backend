2e5b4013dab8bd0bb2801574019a154b0d9673c2
Rostyslav <rostyslav.brahinets@gmail.com>
2023-05-27 03:28:02 +0300

    Update password for admin

--- src/main/resources/db/migration/admin/V20230517170644__Insert_data_to_table_admin.sql
+++ src/main/resources/db/migration/admin/V20230517170644__Insert_data_to_table_admin.sql
@@ -1,2 +1,2 @@
 INSERT INTO `admin` (`first_name`, `last_name`, `email`, `password`)
-VALUES ('John', 'Smith', 'john.smith@carsharing.com', 'admin');
+VALUES ('John', 'Smith', 'john.smith@carsharing.com', '$2a$10$AOVwVltOuiy27SBtwwqid.6gsPN/WTvJgDREGllWnR/f0P5HgiAmO');
