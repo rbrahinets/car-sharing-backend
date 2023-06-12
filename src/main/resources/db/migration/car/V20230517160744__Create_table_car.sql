ded1a34115d9fcef55f5f31a83b7007151b228c4
Rostyslav <rostyslav.brahinets@gmail.com>
2023-06-12 12:59:09 +0300

    Update sql scripts for car

--- src/main/resources/db/migration/car/V20230517160744__Create_table_car.sql
+++ src/main/resources/db/migration/car/V20230517160744__Create_table_car.sql
@@ -1,14 +1,18 @@
 CREATE TABLE IF NOT EXISTS `car`
 (
-    `id`          INT(11)       NOT NULL AUTO_INCREMENT,
-    `brand`       VARCHAR(50)   NOT NULL,
-    `model`       VARCHAR(50)   NOT NULL,
-    `year`        YEAR          NOT NULL,
-    `price`       DECIMAL(10, 2) NOT NULL,
-    `id_category` INT(11)       NOT NULL,
-    `plate`       VARCHAR(10)   NOT NULL UNIQUE,
-    `available`   TINYINT(1)    NOT NULL,
-    `image`       LONGBLOB,
+    `id`            INT(11)        NOT NULL AUTO_INCREMENT,
+    `brand`         VARCHAR(50)    NOT NULL,
+    `model`         VARCHAR(50)    NOT NULL,
+    `year`          YEAR           NOT NULL,
+    `price`         DECIMAL(10, 2) NOT NULL,
+    `id_category`   INT(11)        NOT NULL,
+    `plate`         VARCHAR(10)    NOT NULL UNIQUE,
+    `location_name` VARCHAR(256)   NOT NULL,
+    `coordinates`   VARCHAR(64)    NOT NULL,
+    `available`     TINYINT(1)     NOT NULL,
+    `id_admin`      INT(11)        NOT NULL,
+    `image`         LONGBLOB,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`id_category`) REFERENCES `category` (`id`) ON DELETE CASCADE
+    FOREIGN KEY (`id_category`) REFERENCES `category` (`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id`) ON DELETE CASCADE
 );
